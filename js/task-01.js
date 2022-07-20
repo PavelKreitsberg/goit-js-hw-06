5d1ac183100373f91a4050f6a46966ee7faabac4
Pavel Kreitsberg <pavel.kreitsberg@gmail.com>
2022-07-20 21:28:46 +0300

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,26 @@
+
+const items = document.querySelectorAll('.item')
+
+
+console.log('Number of categories: ', items.length);
+
+items.forEach(value => {
+    console.log(`Category: `)
+    console.log(`Elements: `);
+})
+
+
+
+
+
+
+// res
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
