c04de1a5354e1428688520257174b081a8eaeec5
Pavel Kreitsberg <pavel.kreitsberg@gmail.com>
2022-07-21 20:14:49 +0300

    Solve task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,29 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+// links
+
+const input = document.querySelector('input');
+
+const createBtn = document.querySelector('button[data-create]');
+
+const destroyBtn = document.querySelector('button[data-destroy]');
+
+const box = document.querySelector('#boxes')
+
+// Control
+
+createBtn.addEventListener('click', (event) => {
+  let str = '';
+
+  for (let i = 0; i < Number(input.value); i += 1) {
+     str += `<div class='box--color' style='width: ${30 + i * 10}px; height: ${30 + i * 10}px; background-color: ${getRandomHexColor()}'></div>`;
+  }
+  
+  box.innerHTML = str;
+})
+
+destroyBtn.addEventListener('click', (event) => {
+  box.innerHTML = '';
+})
