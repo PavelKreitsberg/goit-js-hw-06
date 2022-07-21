376a7bbfb234f10af98e59a8e3ac4ec99d323337
Pavel Kreitsberg <pavel.kreitsberg@gmail.com>
2022-07-21 09:20:31 +0300

    Solve task 5

---
+++ js/task-05.js
@@ -0,0 +1,13 @@
+const textInput = document.querySelector('#name-input');
+
+const output = document.querySelector('#name-output');
+
+textInput.addEventListener('input', (event) => {
+    output.textContent = event.currentTarget.value;
+})
+
+textInput.addEventListener('blur', (event) => {
+    if (event.currentTarget.value === '') {
+        output.textContent = 'Anonymous'
+    }
+})
