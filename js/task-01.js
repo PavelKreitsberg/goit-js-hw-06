97594c7432ee3df02777398639d906608c0505f4
Pavel Kreitsberg <pavel.kreitsberg@gmail.com>
2022-07-21 08:17:55 +0300

    Solve task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,26 +1,10 @@
 
 const items = document.querySelectorAll('.item')
 
-
 console.log('Number of categories: ', items.length);
 
 items.forEach(value => {
-    console.log(`Category: `)
-    console.log(`Elements: `);
+    console.log(`Category: ${value.firstElementChild.textContent}`);
+
+    console.log(`Elements: ${value.lastElementChild.children.length}`); 
 })
-
-
-
-
-
-
-// res
-
-// Category: Animals
-// Elements: 4
-
-// Category: Products
-// Elements: 3
-
-// Category: Technologies
-// Elements: 5
