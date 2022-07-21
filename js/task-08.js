2a333edf1574c70ad82d4527024c491bbd986d4a
Pavel Kreitsberg <pavel.kreitsberg@gmail.com>
2022-07-21 10:53:41 +0300

    Solve task 10

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const user = {
+        login: form.elements.email.value,
+        password: form.elements.password.value,
+    }
+
+    if (user.login === '' || user.password === '') {
+        alert('Please, fill the full information')
+    }
+
+    console.log(user);
+
+    form.reset()
+})
